04558d344cd3f1631498cef444784097ec415046
nando-castro <fernando.castro10.master@gmail.com>
2023-01-10 11:08:12 -0300

    finish activity

--- multiplicaAi/src/App.java
+++ multiplicaAi/src/App.java
@@ -1,5 +1,19 @@
 public class App {
+
+    /*
+     * Escreva um código que deve imprimir a tabela de multiplicação desse algum
+     * número fornecido, e deve exibir um cabeçalho com o nome Tabela de
+     * multiplicação de X, onde X é o número a ser multiplicado.
+     */
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero = 2;
+        int multiplicador = 10;
+        System.out.println("----------------------------");
+        System.out.println("Tabela de multiplicação de " + numero);
+        System.out.println("----------------------------");
+        for (int i = 0; i < multiplicador + 1; i++) {
+            System.out.println(i + " * " + numero + " = " + (i * numero));
+        }
     }
 }
